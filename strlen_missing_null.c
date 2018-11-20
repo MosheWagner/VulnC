a5f8be1132810f4bf4590c40f107a56d190a05eb
root <moshe.wagner@gmail.com>
2018-11-20 11:39:37 +0200

    Strlen malloc missing null example

---
+++ strlen_missing_null.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+char * copy_str(char *in_buff)
+{
+	uint32_t size = strlen(in_buff);
+	char * new_buff = malloc(size);
+	memcpy(new_buff, in_buff, size);
+}
+
+int main()
+{
+	char buff[5];
+	scanf("%s", buff);
+	printf("%s", copy_str(buff));
+
+	return 0;
+}
+
