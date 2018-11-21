b799d033c98315ba3138fe83faa658dd483b0554
root <moshe.wagner@gmail.com>
2018-11-21 09:19:05 +0200

    With new bad reads

---
+++ src/bad_read2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+int main()
+{
+    // open self
+    FILE * fd;
+    fd = fopen("bad_reads.c", "r");
+    
+    
+    if (fd == 0)
+    {
+      perror("Error while opening the file.\n");
+      exit(EXIT_FAILURE);
+    }
+    
+	char * buff ;
+        int sz = 100;
+
+        buff = malloc(sz);
+	read(fd, buff, sz);
+
+	printf("%s", buff);
+
+	return 0;
+}
